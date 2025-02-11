e9236fe8ab7b5fa660f9e99d6b1739a6d70ff2ff
joseangel <joseangelchacin@gmail.com>
2025-02-11 12:54:24 +0100

    006 js arreglos

--- MODULO_1/UF_2_JAVASCRIPT/JS/006_login_captcha/main.js
+++ MODULO_1/UF_2_JAVASCRIPT/JS/006_login_captcha/main.js
@@ -1,9 +1,13 @@
 let num = parseInt(prompt("¿Resultado de 9 + 5?"))
 
-if (num != 14) {
-    alert(num + " no es correcto. Vuelve a intentarlo");
-    windows.location.reload();
-} else {
+if (num === 14) {
     alert('Correcto!');
     document.write("Bienvenido");
+    
+    
+} else if (isNaN(num)) {
+    alert("Introduce un NÚMERO");
+   
+} else {
+    alert(num + " no es correcto. Vuelve a intentarlo.");
 }
